f4068bc2706926c9febf5b5a97518280e44154e0
Ilya Sumbatyants <ilya.sumb@gmail.com>
2022-04-23 14:57:48 +0400

    Update example

--- examples/golang-1.18/go.mod
+++ examples/golang-1.18/go.mod
@@ -2,4 +2,4 @@ module lambda
 
 go 1.18
 
-require github.com/hedlx/doless/runtime/golang-1.18 v0.0.0-20220412184828-ddc08d9aa4b4
+require github.com/hedlx/doless/runtime/golang-1.18 v0.0.0-20220423101031-4852b24e945f
--- examples/golang-1.18/main.go
+++ examples/golang-1.18/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"context"
+	"io"
+
 	doless "github.com/hedlx/doless/runtime/golang-1.18"
 )
 
 func main() {
-	doless.Lambda(func(req *doless.Request) (int, string) {
-		return 200, string(req.Payload)
+	doless.Lambda(func(ctx context.Context, req *doless.Request[io.Reader]) (int, interface{}) {
+		return 200, req.Payload
 	})
 }
